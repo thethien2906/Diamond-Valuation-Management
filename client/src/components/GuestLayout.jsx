a671625f34338d6460e3ce5fee3777ef6aeb7ed1
TranTrongAn1 <Cacchuxai@gmail.com>
2024-07-16 01:14:33 +0700

    responesive for GuestLayout

--- client/src/components/GuestLayout.jsx
+++ client/src/components/GuestLayout.jsx
@@ -1,113 +1,148 @@
-import AccountCircleIcon from '@mui/icons-material/AccountCircle';
-import Container from '@mui/material/Container';
-import IconButton from '@mui/material/IconButton';
-import Menu from '@mui/material/Menu';
-import MenuItem from '@mui/material/MenuItem';
-import Typography from '@mui/material/Typography';
 import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
+import {
+  AppBar,
+  Container,
+  Drawer,
+  IconButton,
+  List,
+  ListItemButton,
+  ListItemText,
+  Toolbar,
+  Typography,
+  MenuItem,
+  useMediaQuery,
+  useTheme
+} from '@mui/material';
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import MenuIcon from '@mui/icons-material/Menu';
 
 const GuestLayout = ({ children }) => {
-  const [showContainer, setShowContainer] = useState(true);
   const [anchorEl, setAnchorEl] = useState(null);
   const navigate = useNavigate();
-  
-  const handleMenu = (event) => {
-    setAnchorEl(event.currentTarget);
-  };
-
-  const handleClose = () => {
-    setAnchorEl(null);
-  };
+  const [drawerOpen, setDrawerOpen] = useState(false);
 
   const handleSignIn = () => {
     navigate('/login');
   };
 
+  const toggleDrawer = () => {
+    setDrawerOpen(!drawerOpen);
+  };
+
+  const drawerContent = (
+    <List>
+      <ListItemButton component={Link} to="/" onClick={toggleDrawer}>
+        <ListItemText primary="HOME" />
+      </ListItemButton>
+      <ListItemButton component={Link} to="/about-us-guest" onClick={toggleDrawer}>
+        <ListItemText primary="ABOUT US" />
+      </ListItemButton>
+      <ListItemButton component={Link} to="/consulting-services-guest" onClick={toggleDrawer}>
+        <ListItemText primary="CONSULTING SERVICE" />
+      </ListItemButton>
+      <ListItemButton component={Link} to="/valuation-tool" onClick={toggleDrawer}>
+        <ListItemText primary="VALUATION TOOL" />
+      </ListItemButton>
+      <ListItemButton component={Link} to="/blogs" onClick={toggleDrawer}>
+        <ListItemText primary="BLOGS" />
+      </ListItemButton>
+    </List>
+  );
+
+  const theme = useTheme();
+  const isMdUp = useMediaQuery(theme.breakpoints.up('md'));
+
   return (
     <div className="home-container" style={{ position: 'relative', zIndex: 1 }}>
-      {/* Notification Bar */}
-      <div
-        style={{
-          width: '100%',
-          backgroundColor: 'white',
-          color: 'black',
-          textAlign: 'center',
-          padding: '5px 0', // Smaller padding
-          position: 'fixed',
-          top: '0px',
-          zIndex: 1200,
-          fontSize: '12px', // Smaller font size
-        }}
-      >
-        <Typography variant="body2" style={{ style:'bold', fontFamily: '"Roboto", "Helvetica", "Arial", sans-serif' }}>
-          Notification: New customers who use our service have a high chance to become honor members!
-        </Typography>
-      </div>
-
       {/* Navigation Bar */}
-      <div
-        style={{
-          position: 'fixed',
-          top: showContainer ? '30px' : '-80px', // Adjusted top position
-          width: '100%',
-          zIndex: 1100,
+      <AppBar
+        position="fixed"
+        sx={{
+          backgroundColor: '#021732',
           boxShadow: '0px 2px 4px rgba(0, 0, 0, 0.1)',
           transition: 'top 0.3s',
-          backgroundColor: '#021732',
+          zIndex: (theme) => theme.zIndex.drawer + 1,
         }}
       >
-        <Container maxWidth="lg" disableGutters style={{ display: 'flex', alignItems: 'center', padding: '10px 0' }}>
+        <Toolbar>
           {/* Logo on the left */}
-          <div style={{ flex: '1', display: 'flex', justifyContent: 'flex-start' }}>
+          <div style={{ flex: '1', display: 'flex', justifyContent: 'flex-start',padding:'10px' }}>
             <img
-              style={{ width: '90px', height: 'auto' }}
+              style={{ width: '80px', height: 'auto' }}
               src='LogoHeader.jpg'
               alt='Logo'
             />
           </div>
 
           {/* Navigation items in the middle */}
-          <div style={{ flex: '2', display: 'flex', justifyContent: 'center', gap: '50px' }}>
-            <MenuItem component={Link} to="/" style={{ padding: '12px 12px'}}>
-              <Typography variant="h6" style={{ fontSize: '14px', fontFamily: '"Roboto", "Helvetica", "Arial", sans-serif', color: 'white' }}>
-                HOME
-              </Typography>
-            </MenuItem>
-            <MenuItem component={Link} to="/about-us-guest" style={{ padding: '12px 12px'}}>
-              <Typography variant="h6" style={{ fontSize: '14px', fontFamily: '"Roboto", "Helvetica", "Arial", sans-serif', color: 'white' }}>
-                ABOUT US
-              </Typography>
-            </MenuItem>
-            <MenuItem component={Link} to="/consulting-services-guest" style={{ padding: '12px 12px'}}>
-              <Typography variant="h6" style={{ fontSize: '14px', fontFamily: '"Roboto", "Helvetica", "Arial", sans-serif', color: 'white' }}>
-                CONSULTING SERVICE
-              </Typography>
-            </MenuItem>
-            <MenuItem component={Link} to="/valuation-tool" style={{ padding: '12px 12px'}}>
-              <Typography variant="h6" style={{ fontSize: '14px', fontFamily: '"Roboto", "Helvetica", "Arial", sans-serif', color: 'white' }}>
-                VALUATION TOOL
-              </Typography>
-            </MenuItem>
-            <MenuItem component={Link} to="/blogs" style={{ padding: '12px 12px'}}>
-              <Typography variant="h6" style={{ fontSize: '14px', fontFamily: '"Roboto", "Helvetica", "Arial", sans-serif', color: 'white' }}>
-                BLOGS
-              </Typography>
-            </MenuItem>
-          </div>
+          {isMdUp && (
+            <div style={{ flex: '2', display: 'flex', justifyContent: 'center', gap: '50px' }}>
+              <MenuItem component={Link} to="/" sx={{ padding: '12px 12px', color: 'white', textDecoration: 'none', cursor: 'pointer' }}>
+                <Typography sx={{ fontSize: '14px' }}>
+                  HOME
+                </Typography>
+              </MenuItem>
+              <MenuItem component={Link} to="/about-us-guest" sx={{ padding: '12px 12px', color: 'white', textDecoration: 'none', cursor: 'pointer' }}>
+                <Typography sx={{ fontSize: '14px' }}>
+                  ABOUT US
+                </Typography>
+              </MenuItem>
+              <MenuItem component={Link} to="/consulting-services-guest" sx={{ padding: '12px 12px', color: 'white', textDecoration: 'none', cursor: 'pointer' }}>
+                <Typography sx={{ fontSize: '14px' }}>
+                  CONSULTING SERVICE
+                </Typography>
+              </MenuItem>
+              <MenuItem component={Link} to="/valuation-tool" sx={{ padding: '12px 12px', color: 'white', textDecoration: 'none', cursor: 'pointer' }}>
+                <Typography sx={{ fontSize: '14px' }}>
+                  VALUATION TOOL
+                </Typography>
+              </MenuItem>
+              <MenuItem component={Link} to="/blogs" sx={{ padding: '12px 12px', color: 'white', textDecoration: 'none', cursor: 'pointer' }}>
+                <Typography sx={{ fontSize: '14px' }}>
+                  BLOGS
+                </Typography>
+              </MenuItem>
+            </div>
+          )}
 
           {/* Sign In and Shopping Cart on the right */}
           <div style={{ flex: '1', display: 'flex', justifyContent: 'flex-end', alignItems: 'center', gap: '20px'}} onClick={handleSignIn}>
-            <Typography variant="h6" style={{ fontSize: '14px', fontFamily: '"Roboto", "Helvetica", "Arial", sans-serif', color: 'white', cursor: 'pointer' }}>
+            <Typography variant="h6" sx={{ fontSize: '14px', color: 'white', cursor: 'pointer' }}>
               Sign In
             </Typography>
             <IconButton style={{ color: 'white' }}>
               <ShoppingCartIcon />
             </IconButton>
           </div>
-        </Container>
-      </div>
+
+          {/* Drawer toggle button */}
+          {!isMdUp && (
+            <IconButton
+              edge="end"
+              aria-label="menu"
+              onClick={toggleDrawer}
+              sx={{ color: 'white' }}
+            >
+              <MenuIcon />
+            </IconButton>
+          )}
+        </Toolbar>
+      </AppBar>
+
+      {/* Drawer for medium screens and above */}
+      {!isMdUp && (
+        <Drawer
+          anchor="right"
+          open={drawerOpen}
+          onClose={toggleDrawer}
+          sx={{ '& .MuiDrawer-paper': { width: '250px' } }}
+        >
+          <div onClick={toggleDrawer} onKeyDown={toggleDrawer}>
+            {drawerContent}
+          </div>
+        </Drawer>
+      )}
       
       <Container maxWidth="lg" style={{ marginTop: '100px', marginBottom: '0px', padding: '20px' }}>
         {children}
