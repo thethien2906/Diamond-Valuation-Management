0b2bac1fa42264d7ff9ce35e871f843448380daf
bertholdt <122198422+thethien2906@users.noreply.github.com>
2024-09-23 22:42:56 +0700

    hehe

--- client/src/pages/_graph/GraphLayout.jsx
+++ client/src/pages/_graph/GraphLayout.jsx
@@ -24,7 +24,7 @@ const GraphLayout = () => {
         <ArrowBackIcon />
       </IconButton>
       <Typography variant="h4" gutterBottom sx={{ textAlign: 'center', mb: 4 }}>
-        Diamond Historical Prices
+        Diamond Shapes Historical Prices hehe
       </Typography>
       <Box sx={{ display: 'flex', flexDirection: 'column', gap: 2, alignItems: 'center' }}>
         {shapes.map((shape) => (
